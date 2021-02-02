14548968256e44656581c8b6a4c7bca9381980c3
Denis Augusto <denisgusto@gmail.com>
2021-02-02 08:31:21 -0300

    0006 - Nosso primeiro modelo (Model User)

---
+++ database/migrations/2021_02_02_022426_create_users_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * Class CreateUsersTable.
+ */
+class CreateUsersTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('users', function(Blueprint $table) {
+			$table->increments('id');
+			
+			// People data
+			$table->string('cpf', 20)->unique()->nullable();
+			$table->string('name', 100);
+			$table->string('phone', 20);
+			$table->date('birth')->nullable();
+			$table->char('gender', 1)->nullable();
+			$table->text('notes')->nullable();
+
+			// Auth data
+			$table->string('email', 80)->unique();
+			$table->string('password', 254)->nullable();
+
+			// Permission
+			$table->string('status')->default('active');
+			$table->string('permission')->default('app.user');
+
+			$table->rememberToken();
+
+			$table->timestamps(); // created_at and updated_at
+			$table->softDeletes(); // deleted_at
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table) {
+
+		});
+
+		Schema::drop('users');
+	}
+}
